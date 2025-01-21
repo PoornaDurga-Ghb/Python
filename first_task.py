926311f992d13aa2a8a88f1157527bd0dbbbc882
PoornaDurga-Ghb <pillipoorna123@gmail.com>
2025-01-21 23:28:56 +0530

    My first python task

---
+++ first_task.py
@@ -0,0 +1,43 @@
+# The comments are represented by hashtag(#) and these are ignored by the computer
+
+#operations using numbers
+print(120+120)
+print(120-120)
+print(120*120)
+print(120/120)
+print(120//120)
+print(120**120)
+
+#operations using variables
+me=84
+you=49
+print(me+you)
+print(me-you)
+print(me*you)
+print(me/you)
+print(me//you)
+print(me**you)
+
+#using strings
+print("Magadheera")
+print('Bahubali')
+print("Bahubali-2")
+
+#using len
+print(len("Magadheera"))
+hello="world"
+print(len(hello))
+hello="earth"
+print(len(hello))
+
+# using type operator
+poorna="999.999"
+print(type(poorna))
+
+# using index 
+index_var="okkokadini kaadhu sherkhan..........vandha mandhini okesari rammanu"
+print(index_var[5])
+print(index_var[18])
+
+# using index with range (slicing)
+print(index_var[10:30])
