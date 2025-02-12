e76851203aaf4b5cb71acbcd6fefea6d240705aa
Poorna Durga <pillipoorna123@gmail.com>
2025-02-12 23:00:49 +0530

    Add files via upload

---
+++ basic-form.py
@@ -0,0 +1,7 @@
+def login(**form):
+    for i,j in form.items():
+        print(i,"=",j)
+login(Name='Python')
+login(Password='*****')
+login(user_name='Your name',password='something',DOB="dd-mm-yyyy")
+login(my_name='Poorna Durga',password='password',DOB="12-02-25",Batch="30R")
